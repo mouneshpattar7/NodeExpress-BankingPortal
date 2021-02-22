f62d4216f0b23ef2401109ac46a539a88cd7b8b6
Mounesh Bhaskar Pattar <M1026883@mindtree.com>
2021-02-22 23:07:51 +0530

    Completed module

--- src/app.js
+++ src/app.js
@@ -26,4 +26,24 @@ app.get('/checking', (req, res) => res.render('account', { account: accounts.che
 app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 
 
+
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance -= req.body.amount;
+    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+    writeJSON();
+    res.render('transfer', {message: 'Transfer Completed'});
+});
+
+app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount);
+    writeJSON();
+    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
+});
+
+
+app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
+
+
 app.listen(3000,() => {console.log(`Server is up and running on port :${port}`)});
